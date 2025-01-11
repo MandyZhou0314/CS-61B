ccb8bf684b4a64bfde18fcbeaad2cc640ba3d3a1
Mandy Zhou <qiong_zhou999@163.com>
2025-01-11 16:32:50 +0800

    lab03 fix

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -32,7 +32,7 @@ public class TestBuggyAList {
         AListNoResizing<Integer> L = new AListNoResizing<>();
         BuggyAList<Integer> R = new BuggyAList<>();
 
-        int N = 100000;
+        int N = 10000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
